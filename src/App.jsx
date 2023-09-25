125a2eb33b3708e935a8dbb038de42a19aca577f
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-25 23:04:56 +0530

    reset application

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home'
-import ButtonToggle from './challenges/Button Toggle/ButtonToggle'
-import Counter from './challenges/Simple Counter/Counter'
-import PhoneBook from './challenges/Phone Book/PhoneBook'
-import TicTacToe from './challenges/Tic-Tac-Toe/TicTacToe'
-import ProgressBar from './challenges/Progress Bar/ProgressBar'
-import Pagination from './challenges/Pagination/Pagination'
-import Modal from './challenges/Modal/Modal'
-import DropDown from './challenges/Drop Down/DropDown'
-import Comments from './challenges/Comment Section/Comments'
-import Accordion from './challenges/Accordion/Accordion'
-import DisabledButton from './challenges/Disable Button/DisabledButton'
-import CurrencyConvert from './challenges/Currency Converter/CurrencyConvert'
-import Geolocate from './challenges/useGeolocate/Geolocate'
+import Home from './Home'
+import Navbar from './Navbar'
 
 function App() {
 
   return (
-    <div className="App">
-
+    <div className='app'>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} >
-          <Route path='button-toggle' element={<ButtonToggle />} />
-          <Route path='simple-counter' element={<Counter />} />
-          <Route path='phonebook' element={<PhoneBook />} />
-          <Route path='tictactoe' element={<TicTacToe />} />
-          <Route path='progressbar' element={<ProgressBar />} />
-          <Route path='currencyconvert' element={<CurrencyConvert />} />
-          <Route path='pagination' element={<Pagination />} />
-          <Route path='modal' element={<Modal />} />
-          <Route path='dropdown' element={<DropDown />} />
-          <Route path='comments' element={<Comments />} />
-          <Route path='accordion' element={<Accordion />} />
-          <Route path='disabledbutton' element={<DisabledButton />} />
-          <Route path='usegeolocate' element={<Geolocate />} />
         </Route>
       </Routes>
     </div>
--- src/challenges/Disable Button/DisabledButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DisabledButton = () => {
-  return (
-    <div>Disabled Button</div>
-  )
-}
-
-export default DisabledButton
