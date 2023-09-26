9adfe7203a12e35e678459d989f76b341099f97c
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-26 22:00:27 +0530

    added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,7 +16,15 @@ const Navbar = () => {
 
   return (
     <nav className='navbar'>
-        
+        <Link to='/' className='navbar-logo'>
+          <SiReactos size={30} />
+          <span>Challenges</span>
+        </Link>
+
+        <div className='navbar-item'>
+          <a className='navbar-link' href='https://react.dev/' target='_blank'>docs</a>
+          <a className='navbar-link' href='https://github.com/prudv13/react-challenges' target='_blank'>code</a>
+        </div>
     </nav>
   )
 }
