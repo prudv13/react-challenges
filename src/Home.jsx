9ee4058ef1258c7a0e2e7ae265b8d78efe5ae850
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-26 22:50:53 +0530

    added home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import challenges from '../utils/data';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+    {
+      challenges.map(challenge => (
+        <div className="box" key={challenge.id}>
+          <div className="info">
+            <h3 className="info-title">{challenge.title} - {challenge.id}</h3>
+            <h5 className="info-subtitle">{challenge.subTitle}</h5>
+            <p className="info-description">
+              {challenge.description}
+            </p>
+          </div>
+        </div>
+      ))
+    }
+    </main>
   )
 }
 
---
+++ utils/data.js
@@ -0,0 +1,82 @@
+const challenges = [
+    {
+        id: 1,
+        title: "Challenge",
+        subTitle: "show/hide title",
+        description: "Implement a button to show/hide the text content"
+    },
+    {
+        id: 2,
+        title: "Challenge",
+        subTitle: "stopwatch/timer",
+        description: "Implement a Stopwatch/timmer as shown below with start ,stop and reset button"
+    },
+    {
+        id: 3,
+        title: "Challenge",
+        subTitle: "todo list",
+        description: "Create a todo app with the following criteria. The user should able to add and remove todo items"
+    },
+    {
+        id: 4,
+        title: "Challenge",
+        subTitle: "progress bar",
+        description: "Create a Progress Bar that should fill based on the input percentage value"
+    },
+    {
+        id: 5,
+        title: "Challenge",
+        subTitle: "submit form data",
+        description: "Grab the values from the input fields and print below as per the sample output"
+    },
+    {
+        id: 6,
+        title: "Challenge",
+        subTitle: "tip calculator",
+        description: "create a tip calculator when you and your friends go to a restaurant and pay the bill"
+    },
+    {
+        id: 7,
+        title: "Challenge",
+        subTitle: "max count",
+        description: "Implement a button and timer that will start from 10 and ends at 0 and tracks the counter untill the timer expires and button should disapper once timer expires"
+    },
+    {
+        id: 8,
+        title: "Challenge",
+        subTitle: "lazy loading",
+        description: "Use React Suspense to load components in lazy load fashion"
+    },
+    {
+        id: 9,
+        title: "Challenge",
+        subTitle: "Catch Route(404)",
+        description: "Create a Route that catches all routes which are not under router scope"
+    },
+    {
+        id: 10,
+        title: "Challenge",
+        subTitle: "usePageBottom",
+        description: "Create a custom Hook to detect if the user scrolled to the bottom of the page"
+    },
+    {
+        id: 11,
+        title: "Challenge",
+        subTitle: "usePageBottom",
+        description: "Create a custom Hook to detect if the user scrolled to the bottom of the page"
+    },
+    {
+        id: 12,
+        title: "Challenge",
+        subTitle: "useWindowDimensions",
+        description: "Create a custom hook to track the dimensions of users window while resizing"
+    },
+    {
+        id: 13,
+        title: "Challenge",
+        subTitle: "useFetch",
+        description: "Implement the `useFetch` function that accepts url as a parameter to instantiate api call and returns data"
+    },
+];
+
+export default challenges;
