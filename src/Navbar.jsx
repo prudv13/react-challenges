f8d393c11cede1e4f81e90e2f540e74eb5174f24
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-29 10:07:20 +0530

    show hide toggle text, button

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,7 @@
-import React, { useState } from 'react'
-import {GiHamburgerMenu} from 'react-icons/gi';
-import {AiOutlineClose} from 'react-icons/ai';
 import {SiReactos} from 'react-icons/si';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-
-    const [click, setClick] = useState(false);
-
-    const closeMobileMenu = () => {
-    }
-
-    const handleClick = () => {
-    }
 
   return (
     <nav className='navbar'>
--- src/components/Challenge-1/Challenge1.jsx
+++ src/components/Challenge-1/Challenge1.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import challenges from '../../../utils/data';
 import Challenge1Styles from './challenge1.module.css';
 
 const Challenge1 = () => {
+
+  const [toggle, setToggle] = useState(true);
+
+  const handleToggle = () => {
+    toggle ? setToggle(false) : setToggle(true);
+  }
   return (
     <div>
       <div className={Challenge1Styles.challenge1Container}>
         <h1>{challenges[0].title} - 1</h1>
         <h3>{challenges[0].subTitle}</h3>
         <p>{challenges[0].description}</p>
+      </div>
+
+      <div className={Challenge1Styles.challenge1}>
+        <div className='d-flex justify-content-center'>
+          <button onClick={handleToggle} className='btn btn-outline-dark'>
+          {!toggle ? 'show' : 'hide'}
+          </button>
+        </div>
+        <p className='form-control text-center'>
+          {toggle ? 'Showing Text Content' : "------"}
+        </p>
       </div>
     </div>
   )
